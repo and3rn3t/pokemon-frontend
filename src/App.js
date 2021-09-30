46173a57453c0c537b8d04214f93803f8a825b42
Matthew Anderson <and3rn3t@icloud.com>
2021-09-30 14:12:11 -0500

    got state working in app js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import React, { Component } from "react";
+import { Switch, Link, Route } from "react-router-dom";
+import axios from "axios";
+
 import "./App.css";
 
-function App() {
-  return <div></div>;
+class App extends Component {
+  constructor() {
+    super();
+    this.apiUrl = "http://localhost:3000/api/pokemon";
+    this.state = {
+      pokemons: [],
+    };
+  }
+
+  async componentDidMount() {
+    const response = await axios.get(this.apiUrl);
+    this.setState({ pokemons: response.data.pokemons });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Pokemon Comparator</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
