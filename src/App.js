9b3924eb2e7c9680254c7ba85deeeb45ce67486d
Matthew Anderson <and3rn3t@icloud.com>
2021-10-03 19:36:59 -0500

    edited axios and url parameters

--- src/App.js
+++ src/App.js
@@ -7,25 +7,33 @@ import "./App.css";
 import Pokemon from "./Components/Pokemon/Pokemon";
 import AllPokemon from "./Components/AllPokemon/AllPokemon";
 
+const apiUrl =
+  process.env.REACT_APP_BACKEND_URL ||
+  "https://pokemon-comparator-frontend.herokuapp.com/api";
+
 class App extends Component {
   constructor() {
     super();
-    this.apiUrl = "http://localhost:3000/api/pokemon";
     this.state = {
       pokemons: [],
     };
   }
 
-  async componentDidMount() {
-    const response = await axios.get(this.apiUrl);
-    this.setState({ pokemons: response.data.pokemons });
+  componentDidMount() {
+    axios.get(`${apiUrl}/pokemon`).then((response) => {
+      this.setState({
+        artists: response.data.pokemons,
+      });
+    });
   }
-  
+
   render() {
     return (
       <div className="App">
         <nav>
-          <Link to="/"><h1>Pokemon Comparator</h1></Link>
+          <Link to="/">
+            <h1>Pokemon Comparator</h1>
+          </Link>
         </nav>
         <main>
           <Switch>
