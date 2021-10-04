f2901b1eef84396e220894f6293fdbc9987a04eb
Matthew Anderson <and3rn3t@icloud.com>
2021-10-04 12:29:06 -0500

    added images and updated pokemonjs

--- src/Components/Pokemon/Pokemon.js
+++ src/Components/Pokemon/Pokemon.js
@@ -3,18 +3,40 @@ import React, { Component } from "react";
 class Pokemon extends Component {
   render() {
     const pokemonDetail = this.props.pokemons.find((pokemon) => {
-      return pokemon.id === this.props.match.params.id;
+      return pokemon.pokeId == this.props.match.params.id;
     });
-    // const pokemonStats = pokemonDetail.Stats.map((stat) => {
-    //   return <li key={stat.id}>{stat.statName}</li>;
-    // });
-
-    // console.log(pokemonDetail);
+    const pokemonStats = pokemonDetail.Stats.map((stat) => {
+      return <li key={stat.id}>{stat.statName}: {stat.baseStat}</li>;
+    });
+    const pokemonTypes = pokemonDetail.Types.map((type) => {
+      return <li key={type.id}>{type.typeName}</li>;
+    });
+    const pokemonAbilities = pokemonDetail.Abilities.map((ability) => {
+      return <li key={ability.id}>{ability.abilityName}</li>;
+    });
 
     return (
       <div>
-        <h3>{pokemonDetail.pokemonName}</h3>
-        {/* <ul>{pokemonStats}</ul> */}
+        <h2>{pokemonDetail.pokemonName}</h2>
+        <h3>Important Info:</h3>
+        National Pokedex ID: {pokemonDetail.pokemonId}<br/>
+        Experience: {pokemonDetail.baseExperience}<br/>
+        Height: {pokemonDetail.height} dm<br/>
+        Weight: {pokemonDetail.weight} hg
+
+        <h3>Species Info:</h3>
+        Generation: {pokemonDetail.PokemonSpecy.generation}<br/>
+        Happiness: {pokemonDetail.PokemonSpecy.baseHappiness}<br/>
+        Capture Rate: {pokemonDetail.PokemonSpecy.captureRate}<br/>
+
+        <h3>Types:</h3>
+        <ul>{pokemonTypes}</ul>
+
+        <h3>Abilities:</h3>
+        <ul>{pokemonAbilities}</ul>
+
+        <h3>Stats:</h3>
+        <ul>{pokemonStats}</ul>
       </div>
     );
   }
